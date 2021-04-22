17ac41b76aaecf8ac117abb947fe0aa5e07150a0
kpedrok <pedrokvb@gmail.com>
2021-04-22 09:13:04 -0300

    Add public folder and pages

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
 import express from "express";
+import { createServer } from "http";
+import path from "path";
+import { Server, Socket } from "socket.io";
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+
+// Get public files
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+const http = createServer(app); // Create http protocol
+const io = new Server(http); // Create websocket protocol
+
+io.on("connection", (socket: Socket) => {
+  console.log("Connected ", socket.id);
+});
 
 app.use(express.json());
 app.use(routes);
