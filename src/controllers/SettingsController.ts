b33f00274dba09afcdcc361b399cb64b271951e4
kpedrok <pedrokvb@gmail.com>
2021-04-22 10:01:57 -0300

    Add setting update function

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -16,6 +16,22 @@ class SettingsController {
       });
     }
   }
+
+  async findByUsername(request: Request, response: Response) {
+    const { username } = request.params;
+    const settingsService = new SettingsService();
+    const userSettings = await settingsService.findByUsername(username);
+    return response.json(userSettings);
+  }
+
+  async update(request: Request, response: Response) {
+    const { username } = request.params;
+    const { chat } = request.body;
+
+    const settingsService = new SettingsService();
+    const updateUserSettings = await settingsService.update(username, chat);
+    return response.json(updateUserSettings);
+  }
 }
 
 export { SettingsController };
--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -29,6 +29,23 @@ class SettingsService {
     await this.settingsRepository.save(settings);
     return settings;
   }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepository.findOne({
+      username,
+    });
+    return settings;
+  }
+
+  async update(username: string, chat: boolean) {
+    const settings = await this.settingsRepository
+      .createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", { username })
+      .execute();
+    return `User: ${username} updated chat to ${chat}`;
+  }
 }
 
 export { SettingsService };
